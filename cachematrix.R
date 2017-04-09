3981fcca393c83ba90d2ed88576d3ddcd6d26e5b
sagar <Sagar.raste@gmail.com>
2017-04-09 16:17:51 -0600

    created functions cacheSolve & makeCacheMatrix and tested both

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,80 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix: This function creates a 
+## special "matrix" object that can cache 
+## its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) {i <<- solve}
+  getinverse <- function() i 
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of
+## the special "matrix" returned by 
+## makeCacheMatrix above. 
+## If the inverse has already been calculated
+## (and the matrix has not changed), 
+## then the cachesolve should retrieve the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+## create an invertible 4 X 4 matrix for testing
+nums <- c(2, 1, 0, 0, 3, 0 , 2, 2, 1, 3, -3, 3, 5, 1, 2, 1)
+mnums <- matrix(nums, nrow = 4, ncol = 4)
+
+## call function that will create special matrix
+cachemnums <- makeCacheMatrix(mnums)
+
+## call function that will return inverse matrix
+## from cache when available else will calculate it
+## and return
+cacheSolve(cachemnums)
+
+## RESULT - 
+## first time when your run the functions  
+## for the test matrix you get results
+## like below - 
+## ------------------------------------
+## [,1] [,2] [,3] [,4]
+## [1,]   18  -35  -28    1
+## [2,]    9  -18  -14    1
+## [3,]   -2    4    3    0
+## [4,]  -12   24   19   -1
+## ------------------------------------
+
+## if you run the same cachesolve(cachemnums) again
+## we get following results
+## ------------------------------------
+## getting cached data
+## [,1] [,2] [,3] [,4]
+## [1,]   18  -35  -28    1
+## [2,]    9  -18  -14    1
+## [3,]   -2    4    3    0
+## [4,]  -12   24   19   -1
+## ------------------------------------
+
+## please notice extra row printed with message - getting cached data
+## that proves function is not recalculating the
+## results, its just pulling it from cache.
+
+
+
+
